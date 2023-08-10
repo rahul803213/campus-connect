670c7e3e08c7c90c74897f23b5265e1042b26058
Rahul Kumar <rahulkr705018@gmail.com>
2023-08-11 03:43:54 +0530

    added more parameter in mongoose.connect 3

--- server/dbs/db.js
+++ server/dbs/db.js
@@ -1,29 +1,25 @@
-const mongoose = require('mongoose');
-require('dotenv').config({path:'.env'});
+const mongoose = require("mongoose");
+require("dotenv").config({ path: ".env" });
 
-
-const uri = process.env.mongodb_url
-
+const uri = process.env.mongodb_url;
 
 const db = async () => {
-    try {
-      await mongoose.connect(String(uri), {
-        useNewUrlParser: true,
-        useNewUrlParser: true,
-        useCreateIndex: true
-       
-      });
-      console.log('Connected to MongoDB');
-    } catch (error) {
-      console.error('Error connecting to MongoDB:', error.message);
-    }
-  };
-
+  try {
+    await mongoose.connect(`${uri}`, {
+      useNewUrlParser: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+  }
+};
 
 // Gracefully close the database connection on application termination
-process.on('SIGINT', async () => {
+process.on("SIGINT", async () => {
   await mongoose.connection.close();
-  console.log('Connection to MongoDB closed');
+  console.log("Connection to MongoDB closed");
   process.exit(0);
 });
 
