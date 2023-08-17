7c1b4f0fe237cc2a076e8af048ce5f2ede85f6ed
Rahul Kumar <rahulkr705018@gmail.com>
2023-08-17 11:18:20 +0530

    exportspath map changed

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    exportPathMap: async function (
-        defaultPathMap,
-        { dev, dir, outDir, distDir, buildId }
-      ) {
-        return {
-          '/': { page: '/' },
-          '/reset-password': { page: '/reset-password' },
-          '/reset-password/123': { page: '/reset-password', query: { token: '123' } },
-          }
-      },
+    
 }
 
 module.exports = nextConfig
